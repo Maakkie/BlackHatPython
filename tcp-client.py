c88eb993ca25876234987181977301e146ac64e2
Maakkie <masaki.scott.abe@gmail.com>
2022-05-29 23:10:57 +0900

    fix typo

--- tcp-client.py
+++ tcp-client.py
@@ -3,6 +3,6 @@ import socket
 target_host = "www.google.com"
 target_port = 80
 
-client = scoket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host,target_port))
